9e046aa7185c1197bc53dc44f82840577c0cc1c5
Ana Pilar Barba López <pilarbarba03@gmail.com>
2022-10-14 09:20:32 +0200

    Interfaces realizadas

---
+++ StarWarsAPI/src/app/interfaces/contents.interface.ts
@@ -0,0 +1,9 @@
+
+    export interface Contents {
+        people: string;
+        planets: string;
+        films: string;
+        species: string;
+        vehicles: string;
+        starships: string;
+    }
---
+++ StarWarsAPI/src/app/interfaces/planets.interface.ts
@@ -0,0 +1,24 @@
+
+export interface Planet {
+    name: string;
+    rotation_period: string;
+    orbital_period: string;
+    diameter: string;
+    climate: string;
+    gravity: string;
+    terrain: string;
+    surface_water: string;
+    population: string;
+    residents: string[];
+    films: string[];
+    created: Date;
+    edited: Date;
+    url: string;
+}
+
+export interface PlanetResponse {
+    count: number;
+    next: string;
+    previous?: any;
+    results: Planet[];
+}
