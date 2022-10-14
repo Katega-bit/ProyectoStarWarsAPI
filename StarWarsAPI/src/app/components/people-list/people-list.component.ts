ffafb76b2053a631d241714a6dfcf7250beca1f9
Ana Pilar Barba López <pilarbarba03@gmail.com>
2022-10-14 09:35:57 +0200

    Se han realizado los componentes de especies, personajes y peliculas

--- StarWarsAPI/src/app/components/people-list/people-list.component.ts
+++ StarWarsAPI/src/app/components/people-list/people-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Film } from 'src/app/interfaces/film.interface';
+import { People } from 'src/app/interfaces/people.interface';
 import { PeopleService } from 'src/app/service/people.service';
 
 @Component({
@@ -7,11 +9,43 @@ import { PeopleService } from 'src/app/service/people.service';
   styleUrls: ['./people-list.component.css']
 })
 export class PeopleListComponent implements OnInit {
+  peopleList:  People[] = [];
+  numPages = 0;
 
-  constructor() { }
+  constructor(private peopleService: PeopleService) { }
 
   ngOnInit(): void {
 
+    this.getPeoplePage(1);
+
+
   }
 
+  showImg(people : People){
+
+    let id = people.url.split("/")[5]
+    return `https://starwars-visualguide.com/assets/img/characters/${id}.jpg`
+
+  }
+
+  showMovie(people : People, film : Film){
+    for (let pelicula of people.films) {
+      if(pelicula==film.url){
+        return film.title;
+        }
+      }
+
+      return null;
+  }
+
+  getPeoplePage(page: number) {
+    this.peopleService.getPeople(page).subscribe(resp =>  {
+      this.peopleList = resp.results;
+      this.numPages = Math.ceil(resp.count / 10);
+    });
+  }
+
+  counter() {
+    return new Array(this.numPages);
+  }
 }
