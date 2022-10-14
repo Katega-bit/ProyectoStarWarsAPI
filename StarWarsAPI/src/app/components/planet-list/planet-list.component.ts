d6064ff41509be158f1994ad54e0f6ab773149f8
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-10-14 09:36:25 +0200

    configurados los componentes

--- StarWarsAPI/src/app/components/planet-list/planet-list.component.ts
+++ StarWarsAPI/src/app/components/planet-list/planet-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Planet } from 'src/app/interfaces/planets.interface';
 import { VehiclesService } from 'src/app/service/vehicles.service';
 import { PlanetService } from 'src/app/service/planet.service';
 
@@ -8,12 +9,33 @@ import { PlanetService } from 'src/app/service/planet.service';
   styleUrls: ['./planet-list.component.css']
 })
 export class PlanetListComponent implements OnInit {
-
-  constructor() { }
+  planetList:  Planet[] = [];
+  numPages = 0;
+  constructor(private planetService : PlanetService) { }
 
   ngOnInit(): void {
+    this.getPlanetsPage(1);
 
   }
 
+  showImg(planet : Planet){
+
+    let id = planet.url.split("/")[5]
+    return `https://starwars-visualguide.com/assets/img/planets/${id}.jpg`
+
+  }
+
+
+
+  getPlanetsPage(page: number) {
+    this.planetService.getPlanets(page).subscribe(resp =>  {
+      this.planetList = resp.results;
+      this.numPages = Math.ceil(resp.count / 10);
+    });
+  }
+
+  counter() {
+    return new Array(this.numPages);
+  }
 
 }
--- StarWarsAPI/src/app/components/starship-list/starship-list.component.ts
+++ StarWarsAPI/src/app/components/starship-list/starship-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Starship } from 'src/app/interfaces/starship.interface';
 import { StarshipService } from 'src/app/service/starship.service';
 
 @Component({
@@ -7,12 +8,33 @@ import { StarshipService } from 'src/app/service/starship.service';
   styleUrls: ['./starship-list.component.css']
 })
 export class StarshipListComponent implements OnInit {
-
-  constructor() { }
+  starshipList:  Starship[] = [];
+  numPages = 0;
+  constructor(private starshipService : StarshipService) { }
 
   ngOnInit(): void {
+    this.getStarshipPage(1);
 
   }
 
+  showImg(starship : Starship){
+
+    let id = starship.url.split("/")[5]
+    return `https://starwars-visualguide.com/assets/img/starships/${id}.jpg`
+
+  }
+
+
+
+  getStarshipPage(page: number) {
+    this.starshipService.getStarship(page).subscribe(resp =>  {
+      this.starshipList = resp.results;
+      this.numPages = Math.ceil(resp.count / 10);
+    });
+  }
+
+  counter() {
+    return new Array(this.numPages);
+  }
 
 }
