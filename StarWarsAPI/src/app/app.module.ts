5edc879691823d5825b1c0303ee57bc3bd7c245d
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-10-14 08:40:35 +0200

    imports

--- StarWarsAPI/src/app/app.module.ts
+++ StarWarsAPI/src/app/app.module.ts
@@ -4,6 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ContentsComponent } from './components/contents/contents.component';
+import { FilmListComponent } from './components/film-list/film-list.component';
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { PlanetListComponent } from './components/planet-list/planet-list.component';
+import { SpeciesListComponent } from './components/species-list/species-list.component';
+import { StarshipListComponent } from './components/starship-list/starship-list.component';
+import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
+import { MaterialsImportModule } from './materials-import/materials-import.module';
 
 
 @NgModule({
